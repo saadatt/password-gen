068e5bf19180e3c17f0f51ca08309a67641fc4af
Sadat Toktosunova <58871113+saadatt@users.noreply.github.com>
2022-04-01 20:26:57 -0500

    Update interviewNextiva.js

--- interviewNextiva.js
+++ interviewNextiva.js
@@ -13,7 +13,7 @@ function generatePassword () {
     const uppercaseLetters = lowercaseLetters.toUpperCase();
     const specialCharacters = '!@$%^&';
     const digits = '0123456789';
-    let resultString ='';
+    let resultCharArray =[];
 
     function getRandomNumberInInterval(min, max) {
         return Math.floor(Math.random() * (max-min) + min);
@@ -21,12 +21,33 @@ function generatePassword () {
     const passwordLength = getRandomNumberInInterval(minLength, maxLength)
 
     for (let i = 0; i <= passwordLength; ++i ) {
-        if (i % 4 === 0)
-            resultString += lowercaseLetters.charAt(getRandomNumberInInterval(0, lowercaseLetters.length));
-        else if (i % 3 === 0) resultString += uppercaseLetters.charAt(getRandomNumberInInterval(0, uppercaseLetters.length));
-        else if (i % 2 === 0) resultString += specialCharacters.charAt(getRandomNumberInInterval(0,specialCharacters.length));
-        else resultString += digits.charAt(getRandomNumberInInterval(0, digits.length));
-    } return resultString;
+        let randomizeGroup = getRandomNumberInInterval(0, 4);
+        
+        if (randomizeGroup === 0)
+            resultCharArray.push(lowercaseLetters.charAt(getRandomNumberInInterval(0, lowercaseLetters.length)));
+        else if (randomizeGroup === 1) resultCharArray.push(uppercaseLetters.charAt(getRandomNumberInInterval(0, uppercaseLetters.length)));
+        else if (randomizeGroup === 2) resultCharArray.push(specialCharacters.charAt(getRandomNumberInInterval(0,specialCharacters.length)));
+        else resultCharArray.push(digits.charAt(getRandomNumberInInterval(0, digits.length)));
+    } 
+    let lowercaseIndex = getRandomNumberInInterval(0, passwordLength);
+    resultCharArray[lowercaseIndex] = lowercaseLetters.charAt(getRandomNumberInInterval(0, lowercaseLetters.length));
+    let uppercaseIndex = getRandomNumberInInterval(0, passwordLength);
+    while(uppercaseIndex === lowercaseIndex) {
+        uppercaseIndex = getRandomNumberInInterval(0, passwordLength);
+    }
+    resultCharArray[uppercaseIndex] = uppercaseLetters.charAt(getRandomNumberInInterval(0, uppercaseLetters.length));
+    let specialIndex = getRandomNumberInInterval(0, passwordLength);
+    while(specialIndex === lowercaseIndex || specialIndex === uppercaseIndex) {
+        specialIndex = getRandomNumberInInterval(0, passwordLength);
+    }
+    resultCharArray[specialIndex] = specialCharacters.charAt(getRandomNumberInInterval(0,specialCharacters.length));
+    let digitIndex = getRandomNumberInInterval(0, passwordLength);
+    while(digitIndex === lowercaseIndex || digitIndex === uppercaseIndex || digitIndex === specialIndex) {
+        specialIndex = getRandomNumberInInterval(0, passwordLength);
+    }
+    resultCharArray[digitIndex] = digits.charAt(getRandomNumberInInterval(0, digits.length));
+    
+    return resultCharArray.join('');
 }
 
 
